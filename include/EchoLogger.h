051d0b2de56fa0497c386446a244a2a4e5354a00
S. Tyler Berryhill <sean.tyler.berryhill@gmail.com>
2022-10-17 23:27:37 -0500

    Add CMake build system.

--- include/EchoLogger.h
+++ include/EchoLogger.h
@@ -1,16 +1,16 @@
 #ifndef _ECHO_LOGGER_H
 #define _ECHO_LOGGER_H
 
-#include "EchoAPIMacro.h"
+#include "EchoLoggerAPIMacro.h"
 #include <stdbool.h>
 
 typedef struct Logger Logger;
 
-ECHO_API Logger *Logger_OpenFileLogger(const char *fileName, bool outputToTerminal);
-ECHO_API Logger *Logger_OpenConsoleLogger();
-ECHO_API void Logger_Info(const Logger *logger, const char *message, ...);
-ECHO_API void Logger_Warn(const Logger *logger, const char *message, ...);
-ECHO_API void Logger_Error(const Logger *logger, const char *message, ...);
-ECHO_API void Logger_Close(Logger *logger);
+ECHO_LOGGER_API Logger *Logger_OpenFileLogger(const char *fileName, bool outputToTerminal);
+ECHO_LOGGER_API Logger *Logger_OpenConsoleLogger();
+ECHO_LOGGER_API void Logger_Info(const Logger *logger, const char *message, ...);
+ECHO_LOGGER_API void Logger_Warn(const Logger *logger, const char *message, ...);
+ECHO_LOGGER_API void Logger_Error(const Logger *logger, const char *message, ...);
+ECHO_LOGGER_API void Logger_Close(Logger *logger);
 
 #endif
